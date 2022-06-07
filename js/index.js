6111e3efc239909fce0c8627ed85a13a60370bff
qfqha <60004573+qfqha@users.noreply.github.com>
2022-06-07 20:45:47 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="胡涵益" &&  pwd=="412"){
+    if(userName=="李锦超" &&  pwd=="603"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
